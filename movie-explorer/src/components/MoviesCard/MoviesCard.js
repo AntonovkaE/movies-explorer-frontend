b44ffde4556c536897690449f1dc122b4334c232
Elvira <sabirova.elvira.gam@gmail.com>
2022-09-26 10:17:02 +0500

    feat: add styles for Movie

--- movie-explorer/src/components/MoviesCard/MoviesCard.js
+++ movie-explorer/src/components/MoviesCard/MoviesCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import first from '../../images/1.jpg'
+import './MoviesCard.css';
 import Button from '../Button/Button';
 
-function MovieCard() {
+function MovieCard({ srcImage }) {
   return (
     <li className="movie">
-      <article>
-        <h2 className="movie__title">В погоне за Бенкси</h2>
-        <p className="movie__duration">27 минут</p>
-        <img className="movie__img" alt="кадр фильма" src={first}/>
+      <article className="movie__article">
+        <div className="movie__header">
+          <h2 className="movie__title">В погоне за Бенкси</h2>
+          <p className="movie__duration">27 минут</p>
+        </div>
+        <img className="movie__img" alt="кадр фильма" src={srcImage}/>
         <Button type="movie" status="saved"></Button>
       </article>
     </li>
--- movie-explorer/src/components/MoviesCardList/MoviesCardList.js
+++ movie-explorer/src/components/MoviesCardList/MoviesCardList.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import './MoviesCardList.css';
 import MovieCard from '../MoviesCard/MoviesCard';
+import first from '../../images/1.jpg';
+import second from '../../images/2.jpg'
+import third from '../../images/3.jpg'
+import forth from '../../images/4.jpg'
+import Preloader from '../Preloader/Preloader';
 
 function MovieCardList() {
   return (
     <section className="section section_movieCardList" aria-label="Фильмы">
+      {/*<Preloader/>*/}
       <ul className="list movies">
-        <MovieCard key="1" />
+        <MovieCard key="1" srcImage={first}/>
+        <MovieCard key="2" srcImage={second}/>
+        <MovieCard key="3" srcImage={third}/>
+        <MovieCard key="4" srcImage={forth}/>
+        <MovieCard key="1" srcImage={first}/>
+        <MovieCard key="2" srcImage={second}/>
+        <MovieCard key="3" srcImage={third}/>
+        <MovieCard key="4" srcImage={forth}/>
+        <MovieCard key="1" srcImage={first}/>
+        <MovieCard key="2" srcImage={second}/>
+        <MovieCard key="3" srcImage={third}/>
+        <MovieCard key="4" srcImage={forth}/>
+        <MovieCard key="1" srcImage={first}/>
+        <MovieCard key="2" srcImage={second}/>
+        <MovieCard key="3" srcImage={third}/>
+        <MovieCard key="4" srcImage={forth}/>
         {/*{movies.map((movie, i) => (*/}
         {/*  <MovieCard key={movie._id} movie={movie}/>*/}
         {/*))}*/}
       </ul>
+      <button className="button button_showMore">Ещё</button>
 
     </section>
   );
