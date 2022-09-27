2a02ff8f4fbfa9428533e8940e306c4516a342c9
Elvira <sabirova.elvira.gam@gmail.com>
2022-09-27 09:24:13 +0500

    feat: add header's styles for signup and signin

--- movie-explorer/src/components/Header/Header.js
+++ movie-explorer/src/components/Header/Header.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
 import accountIcon from '../../images/account-icon.svg';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Header.css';
 
 function Header({ isAuth }) {
+  const location = useLocation().pathname;
+  console.log(location);
   const showMenu = () => {
   };
+  const isNavigationHidden = (location === '/signup' || location === '/signin');
 
   return (
     <header className={`header`}>
-      <nav className="header__nav">
+      <nav className={`header__nav ${isNavigationHidden ? 'header__nav_center' : ''}`}>
         <img className="header__logo" alt="logo" src={logo}/>
         <div onClick={showMenu}
-             className={`header__burger ${!isAuth ? 'header__burger_hidden' : ''}  burger`}>
+             className={`header__burger ${(!isAuth || isNavigationHidden) ? 'header__burger_hidden' : ''} burger`}>
           <div className="burger__line burger__line_first"></div>
           <div className="burger__line burger__line_second"></div>
           <div className="burger__line burger__line_third"></div>
         </div>
         <ul
-          className={`header__menu menu list ${isAuth ? 'header__menu_hidden' : 'header__menu_notAuth'}`}>
+          className={`header__menu menu list ${(isAuth || isNavigationHidden) ? 'header__menu_hidden' : 'header__menu_notAuth'}`}>
           <li className="menu__item">
             <NavLink className="menu__link link" to="/sign-in">Регистрация</NavLink>
           </li>
@@ -28,7 +31,7 @@ function Header({ isAuth }) {
           </li>
         </ul>
         <ul
-          className={`list header__menu  ${!isAuth ? 'header__menu_hidden' : 'header__menu_auth'} `}>
+          className={`list header__menu  ${(!isAuth || isNavigationHidden) ? 'header__menu_hidden' : 'header__menu_auth'} `}>
           <li className="menu__item"><NavLink className="menu__link link"
                                               to="/movies">Фильмы</NavLink></li>
           <li className="menu__item"><NavLink className="menu__link link" to="/saved-movies">Сохранённые
